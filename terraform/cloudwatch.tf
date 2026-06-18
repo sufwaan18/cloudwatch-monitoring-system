09eaca69bfd0d1e34bce937c5b17c74db1482758
Sufwaan-Amees <151483283+sufwaan18@users.noreply.github.com>
2026-06-17 19:04:52 -0500

    Create cloudwatch.tf

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
+  alarm_name          = "high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+}
